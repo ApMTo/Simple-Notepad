e44902cc9b1ee0fcbae9918837754dbd9158dbcf
ApMTo <edosargsyan891@gmail.com>
2025-01-18 17:30:31 +0400

    fixed categories bug

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -3,19 +3,22 @@ import {
   deletePost,
   selectCategories,
 } from "../../store/slices/Categories/slice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import styles from "./category.module.css";
 import { deletePostLocal } from "../../utils/storageManager";
 
 const Category = () => {
   const { currentCategory } = useSelector(selectCategories);
+  const [isOk, setIsOk] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (!currentCategory?.name) {
-      navigate("/");
+      setIsOk(false);
+    } else {
+      setIsOk(true);
     }
   }, [currentCategory, navigate]);
 
@@ -27,45 +30,68 @@ const Category = () => {
   return (
     <>
       <Outlet />
-      <div className={styles.goBack}>
-        <Link to="/">
-          <i className="fa-solid fa-arrow-left"></i>
-        </Link>
-      </div>
-      <div className={styles.main}>
-        <div className={styles.section}>
-          <div className={styles.header}>
-            <h2>{currentCategory?.name}</h2>
-            <Link to="add">
-              <i className="fa-solid fa-plus"></i>
+      {isOk ? (
+        <>
+          <div className={styles.goBack}>
+            <Link to="/">
+              <i className="fa-solid fa-arrow-left"></i>
             </Link>
           </div>
-          <div className={styles.description}>
-            <i className="fa-solid fa-exclamation"></i>
-            <span>{currentCategory?.advice}</span>
+          <div className={styles.main}>
+            <div className={styles.section}>
+              <div className={styles.header}>
+                <h2>{currentCategory?.name}</h2>
+                <Link to="add">
+                  <i className="fa-solid fa-plus"></i>
+                </Link>
+              </div>
+              <div className={styles.description}>
+                <i className="fa-solid fa-exclamation"></i>
+                <span>{currentCategory?.advice}</span>
+              </div>
+              <div className={styles.posts}>
+                {currentCategory?.posts?.length > 0
+                  ? currentCategory.posts.map((el, index) => (
+                      <div key={index} className={styles.container}>
+                        <div className={styles.date}>
+                          <span>
+                            Date: {el.date}{" "}
+                            <i
+                              className="fa-solid fa-trash"
+                              onClick={() => handlerDeletePost(el.id)}
+                            ></i>
+                          </span>
+                        </div>
+                        <div className={styles.text}>
+                          <span>Text: {el.text}</span>
+                        </div>
+                      </div>
+                    ))
+                  : ""}
+              </div>
+            </div>
           </div>
-          <div className={styles.posts}>
-            {currentCategory?.posts?.length > 0
-              ? currentCategory.posts.map((el, index) => (
-                  <div key={index} className={styles.container}>
-                    <div className={styles.date}>
-                      <span>
-                        Date: {el.date}{" "}
-                        <i
-                          className="fa-solid fa-trash"
-                          onClick={() => handlerDeletePost(el.id)}
-                        ></i>
-                      </span>
-                    </div>
-                    <div className={styles.text}>
-                      <span>Text: {el.text}</span>
-                    </div>
-                  </div>
-                ))
-              : ""}
+        </>
+      ) : (
+        <>
+          <div className={styles.goBack}>
+            <Link to="/">
+              <i className="fa-solid fa-arrow-left"></i>
+            </Link>
           </div>
-        </div>
-      </div>
+          <div className={styles.main}>
+            <div className={styles.section}>
+              <div className={styles.header}>
+                <h2>{currentCategory?.name}</h2>
+              </div>
+              <div className={styles.description}>
+                <i className="fa-solid fa-exclamation"></i>
+                <span>Error, go back and click on the category again</span>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
